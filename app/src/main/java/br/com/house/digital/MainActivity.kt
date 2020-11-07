f0acfb25ad41722d39702349b4cfe5435bcbc4df
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-11-07 09:22:12 -0300

    template base

---
+++ app/src/main/java/br/com/house/digital/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.house.digital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.house.digital.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
